1a8113ca6db06c61d9a4ef6373ee62d3a7ba1572
Ryoichiro Kamiya <Ryo@MacBook-Pro.local>
2016-03-02 16:38:20 +0900

    Fix AppRegistryNotReady error.

--- tos/models.py
+++ tos/models.py
@@ -3,14 +3,14 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 # Django 1.4 compatability
+from django.conf import settings
 try:
     from django.contrib.auth import get_user_model
+    USER_MODEL = settings.AUTH_USER_MODEL
 except ImportError:
     from django.contrib.auth.models import User
     get_user_model = lambda: User
-
-
-USER_MODEL = get_user_model()
+    USER_MODEL = get_user_model()
 
 
 class NoActiveTermsOfService(ValidationError):
